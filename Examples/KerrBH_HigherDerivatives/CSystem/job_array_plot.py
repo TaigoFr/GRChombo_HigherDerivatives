bcae421b4715167bcf4bfb6120bab68bbc22e4b5
TaigoFr <taigo.fr@outlook.com>
2021-03-12 12:44:02 +0000

    Add job array summary and plot script Fix job array setup

---
+++ Examples/KerrBH_HigherDerivatives/CSystem/job_array_plot.py
@@ -0,0 +1,72 @@
+# this file is intended to plot the summary of the result of an array job from the 'job_array_summary.py'
+
+# use as 'python job_array_plot.py summary_file.txt'
+
+import sys # to get command line arguments
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+if len(sys.argv)>1:
+    summary_filename = sys.argv[1]
+
+    summary_file = open(summary_filename, 'r')
+
+    lines = summary_file.readlines()
+
+    content = []
+    for line in lines:
+        if line[0]!="#" and len(line) > 3:
+            content.append(json.loads(line))
+
+    params_to_vary = content[0]
+    params = np.array(content[1])
+    success = np.array(content[2])
+    final_times = np.array(content[3])
+
+    # PLOT OF FINAL TIME
+
+    y0 = params[0][1]
+    ny = 1
+    for i in range(1,len(params)):
+        if params[i][1] == y0:
+            ny += 1
+
+    nx = len(params)//ny
+
+    xs = np.log2(params[:,0])
+    ys = np.log2(params[:,1])
+
+    xs_split = np.array(np.split(xs, ny))
+    ys_split = np.array(np.split(ys, nx))
+    final_times_split = np.array(np.split(final_times, ny))
+
+    fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    ax = Axes3D(fig)
+    ax.plot_surface(xs_split, ys_split, final_times_split)
+
+    ax.set_xlabel("log2(" + params_to_vary[0] + ")")
+    ax.set_ylabel("log2(" + params_to_vary[1] + ")")
+    ax.set_zlabel("Duration of run")
+
+    # plt.show()
+    plt.close()
+
+    # PLOT OF AH FINAL MASS
+    if len(content) >= 6:
+        ah_masses = np.array(content[5])
+
+        valid_x = xs[success]
+        valid_y = ys[success]
+        valid_ah_masses = ah_masses[success]
+
+        if len(valid_ah_masses) > 0:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(valid_x, valid_y, valid_ah_masses)
+            plt.show()
+
+else:
+    print("Run as 'python job_array_plot.py summary_file.txt'")
--- Examples/KerrBH_HigherDerivatives/CSystem/job_array_setup.py
+++ Examples/KerrBH_HigherDerivatives/CSystem/job_array_setup.py
@@ -2,7 +2,7 @@
 # (e.g. tau and sigma)
 
 # it will create a file 'specific_params.txt' for the index 'i' of an array-job
-# Run as: 'python job_array_helper.py index total', where index is the index in the array, of a total of 'total' jobs
+# Run as: 'python job_array_setup.py index total', where index is the index in the array, of a total of 'total' jobs
 # Then, run GRChombo as 'MainExample.ex params.txt FILE=specific_params.txt'
 
 
@@ -14,10 +14,10 @@ params_max     = [1. , 1.]
 params_ratio   = [0.5 , 0.5] # search range is [1, 0.5, 0.25, 0.125, ...]
 
 if len(sys.argv)>2:
-    index = int(sys.argv[1])
+    index = int(sys.argv[1]) # indices in job arrays start in 1
     total = int(sys.argv[2])
 
-    assert(index >= 0 and index < total)
+    assert(index > 0 and index <= total)
 
     num_params = len(params_to_vary)
 
@@ -30,23 +30,27 @@ if len(sys.argv)>2:
         print("Total %d is not a perfect square." % total)
     print("Using %d jobs for each of the %d parameters, for a total of %d." % (total_per_params, num_params, actual_total))
 
-    if index >= actual_total:
+    if index > actual_total:
         print('Not running index %d bigger than the actual total to run %d' % (index, actual_total))
         exit()
 
-    sub_total_index = index
+    sub_total_index = index - 1
     indices = [0]*num_params
     params = [0]*num_params
     file = open('specific_params.txt', 'w')
+    file.write("# file generated automatically for index %d / %d\n\n" % (index, actual_total))
     for p in range(num_params):
         indices[p] = sub_total_index % total_per_params
         sub_total_index = int(sub_total_index / total_per_params)
         params[p] = params_max[p] * math.pow(params_ratio[p], indices[p])
-        file.write(params_to_vary[p] + " = " + str(params[p]) + "\n")
+        file.write("# Parameter %d:\n" % p)
+        file.write("# Index = %d\n" % indices[p])
+        file.write("# Using param_max = %f and param_ratio = %f\n" % (params_max[p], params_ratio[p]))
+        file.write(params_to_vary[p] + " = " + str(params[p]) + "\n\n")
 
     print("Indices    for this job:", indices)
     print("Parameters for this job:", params)
     file.close()
 
 else:
-    print("Run as 'python job_array_helper.py index total'")
+    print("Run as 'python job_array_setup.py index total'")
